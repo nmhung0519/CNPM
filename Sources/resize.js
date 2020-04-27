d33d92fb1780f351c321d2ce0c31525baa03573c
huycay278 <61721891+huycay278@users.noreply.github.com>
2020-04-27 09:49:30 +0700

    Update resize.js

--- Sources/resize.js
+++ Sources/resize.js
@@ -1,5 +1,7 @@
 window.addEventListener('resize', fixPosition);
 function fixPosition() {
-    document.getElementById("container").style.top = (window.innerHeight - 600)/3 + "px";
-    document.getElementById("container").style.left = (window.innerWidth - 1000)/2 + "px";
+    if (window.innerWidth > 1000) document.getElementById("container").style.left = (window.innerWidth - 1000) / 2 + "px";
+    else document.getElementById("container").style.left = "0px";
+    if (window.innerHeight > 600) document.getElementById("container").style.top = (window.innerHeight - 600) / 3 + "px";
+    else document.getElementById("container").style.top = "0px";
 }
