2fea56cfdc920a88c72bf55a41402c4d302106fd
Lê Hạnh <lehanh9a@gmail.com>
2020-05-26 11:30:48 +0700

    Check code smell and fix screen outline

--- Sources/mouse_on_startButton.js
+++ Sources/mouse_on_startButton.js
@@ -1,21 +1,23 @@
-var start_button = document.getElementById("start_button");
-var start_button_1 = document.getElementById("start_button_1");
-var start_img = document.getElementById("start_img");
-function over() {
-    start_button.style.left = "420px";
-    start_button.style.top = "295px"
-    start_button.style.width = "160px";
-    start_button.style.height = "160px";
-    start_button_1.style.left= "20px";
-    start_button_1.style.top= "20px";
-    start_img.src = "../Picture/Start1.png";
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+var start_button = document.getElementById('start_button')
+var start_button_1 = document.getElementById('start_button_1')
+var start_img = document.getElementById('start_img')
+function over () {
+  start_button.style.left = '420px'
+  start_button.style.top = '295px'
+  start_button.style.width = '160px'
+  start_button.style.height = '160px'
+  start_button_1.style.left = '20px'
+  start_button_1.style.top = '20px'
+  start_img.src = '../Picture/Start1.png'
 }
-function out() {
-    start_button.style.left= "425px";
-    start_button.style.top = "300px";
-    start_button.style.width = "150px";
-    start_button.style.height = "150px";
-    start_button_1.style.left= "15px";
-    start_button_1.style.top= "15px";
-    start_img.src = "../Picture/Start0.png";
+function out () {
+  start_button.style.left = '425px'
+  start_button.style.top = '300px'
+  start_button.style.width = '150px'
+  start_button.style.height = '150px'
+  start_button_1.style.left = '15px'
+  start_button_1.style.top = '15px'
+  start_img.src = '../Picture/Start0.png'
 }
--- Sources/resize.js
+++ Sources/resize.js
@@ -1,7 +1,7 @@
-window.addEventListener('resize', fixPosition);
-function fixPosition() {
-    if (window.innerWidth > 1000) document.getElementById("container").style.left = (window.innerWidth - 1000) / 2 + "px";
-    else document.getElementById("container").style.left = "0px";
-    if (window.innerHeight > 600) document.getElementById("container").style.top = (window.innerHeight - 600) / 3 + "px";
-    else document.getElementById("container").style.top = "0px";
+window.addEventListener('resize', fixPosition)
+function fixPosition () {
+  if (window.innerWidth > 1000) document.getElementById('container').style.left = (window.innerWidth - 1000) / 2 + 'px'
+  else document.getElementById('container').style.left = '0px'
+  if (window.innerHeight > 600) document.getElementById('container').style.top = (window.innerHeight - 600) / 3 + 'px'
+  else document.getElementById('container').style.top = '0px'
 }
